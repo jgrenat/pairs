27917733069e579386f20646907ddf339f82f74b
Merlin Pahic <35031144+O-O-Balance@users.noreply.github.com>
2019-06-14 16:15:44 +0200

    Create UI elements
    - Grid of buttons as visual representation for the cards
    - Restart button (only visible in the Solved state)

--- src/pairs.elm
+++ src/pairs.elm
@@ -1,7 +1,9 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, text)
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+import List.Extra as List
 
 
 type alias Model =
@@ -27,6 +29,7 @@ type Msg
     = Click Int
     | TimeOut
     | Shuffle (List Int)
+    | Restart
 
 
 main =
@@ -73,4 +76,56 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    div [] [ text "Nothing to see here" ]
+    let
+        columns =
+            calculateColumns (List.length model.cards)
+    in
+        div []
+            (createHeader model.state
+                :: List.map createRow (List.groupsOf columns model.cards)
+            )
+
+
+{-| Try for equal number of rows and columns,
+favoring more columns if numCards is not a perfect square
+-}
+calculateColumns : Int -> Int
+calculateColumns numCards =
+    Maybe.withDefault numCards
+        (List.find
+            (\n -> modBy n numCards == 0)
+            (List.range
+                (numCards
+                    |> toFloat
+                    |> sqrt
+                    |> ceiling
+                )
+                numCards
+            )
+        )
+
+
+createHeader : State -> Html Msg
+createHeader state =
+    div []
+        (case state of
+            Solved ->
+                [ text "Congratulations!"
+                , button [ onClick Restart ] [ text "Play again" ]
+                ]
+
+            other ->
+                [ text "Click on the cards to reveal them" ]
+        )
+
+
+createRow : List Int -> Html Msg
+createRow cards =
+    div [] (List.map createButton cards)
+
+
+createButton : Int -> Html Msg
+createButton card =
+    button
+        [ onClick (Click card) ]
+        [ text (String.fromInt card) ]
