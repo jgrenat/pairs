f3a44f5fde9a03cf59fcd5c51765debe76101592
O.O.Balance <merlin.pahic@eidu.com>
2020-05-17 23:24:02 +0200

    Reformat Main.elm using elm-format

--- src/Main.elm
+++ src/Main.elm
@@ -57,11 +57,11 @@ initialModel =
         numPairs =
             10
     in
-        { state = InProgress Hidden
-        , numPairs = numPairs
-        , cards = List.range 1 (numPairs * 2)
-        , matched = Set.empty
-        }
+    { state = InProgress Hidden
+    , numPairs = numPairs
+    , cards = List.range 1 (numPairs * 2)
+    , matched = Set.empty
+    }
 
 
 shuffleCards : List Int -> Cmd Msg
@@ -85,6 +85,7 @@ update msg model =
         Click card ->
             ( if Set.member card model.matched then
                 model
+
               else
                 case model.state of
                     InProgress Hidden ->
@@ -95,20 +96,23 @@ update msg model =
                             matched =
                                 if matching model card1 card then
                                     Set.insert card1 model.matched |> Set.insert card
+
                                 else
                                     model.matched
                         in
-                            if card == card1 then
-                                model
-                            else
-                                { model
-                                    | state =
-                                        if Set.size matched == model.numPairs * 2 then
-                                            Solved
-                                        else
-                                            InProgress (TwoRevealed card1 card)
-                                    , matched = matched
-                                }
+                        if card == card1 then
+                            model
+
+                        else
+                            { model
+                                | state =
+                                    if Set.size matched == model.numPairs * 2 then
+                                        Solved
+
+                                    else
+                                        InProgress (TwoRevealed card1 card)
+                                , matched = matched
+                            }
 
                     other ->
                         model
@@ -147,10 +151,10 @@ view model =
         columns =
             calculateColumns (List.length model.cards)
     in
-        div []
-            (createHeader model
-                :: List.map (createRow model) (List.groupsOf columns model.cards)
-            )
+    div []
+        (createHeader model
+            :: List.map (createRow model) (List.groupsOf columns model.cards)
+        )
 
 
 {-| Try for equal number of rows and columns,
@@ -185,6 +189,7 @@ createHeader model =
                 [ text
                     (if matching model card1 card2 then
                         "A match!"
+
                      else
                         "Not a match, try again"
                     )
@@ -207,6 +212,7 @@ createButton model card =
         , disabled
             (if Set.member card model.matched then
                 True
+
              else
                 case model.state of
                     InProgress (TwoRevealed _ _) ->
@@ -228,21 +234,24 @@ buttonText model number =
         text =
             String.fromInt (modBy (List.length model.cards // 2) number)
     in
-        if Set.member number model.matched then
-            text
-        else
-            case model.state of
-                InProgress (OneRevealed card1) ->
-                    if card1 == number then
-                        text
-                    else
-                        "X"
+    if Set.member number model.matched then
+        text
 
-                InProgress (TwoRevealed card1 card2) ->
-                    if List.member number [ card1, card2 ] then
-                        text
-                    else
-                        "X"
+    else
+        case model.state of
+            InProgress (OneRevealed card1) ->
+                if card1 == number then
+                    text
 
-                other ->
+                else
                     "X"
+
+            InProgress (TwoRevealed card1 card2) ->
+                if List.member number [ card1, card2 ] then
+                    text
+
+                else
+                    "X"
+
+            other ->
+                "X"
